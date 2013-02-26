ee56718036255fb35001354261fb2ba5c08e1a95
JuneHyeon Bae <devunt@gmail.com>
2013-02-27 04:36:38 +0900

    added (-v, --verbose) and (-c, --count)

--- warp.py
+++ warp.py
@@ -31,29 +31,49 @@ OTHER DEALINGS IN THE SOFTWARE.
 from threading import Thread
 from Queue import Queue
 from socket import (AF_INET, IPPROTO_TCP, SO_REUSEADDR, SOCK_STREAM,
-                    SOL_SOCKET, TCP_NODELAY, socket)
+                    SOL_SOCKET, TCP_NODELAY, socket, error)
 from re import compile
 from time import sleep
+from sys import stdout, stderr
+from datetime import datetime
 from optparse import OptionParser
 
+INFO = 0
+DEBUG = 1
+ERROR = 2
+
 REGEX_HOST = compile(r'(^:+):([0-9]{1,5})')
-REGEX_CONTENT_LENGTH = compile(r'$Content-Length: ([0-9]+)^')
-REGEX_PROXY_CONNECTION = compile(r'$Proxy-Connection: (.+)^')
+REGEX_CONTENT_LENGTH = compile(r'\r\nContent-Length: ([0-9]+)\r\n')
 REGEX_PROXY_CONNECTION = compile(r'\r\nProxy-Connection: (.+)\r\n')
-REGEX_CONNECTION = compile(r'$Connection: (.+)^')
+REGEX_CONNECTION = compile(r'\r\nConnection: (.+)\r\n')
+
+flag = INFO
+
+def _log(message, where=stdout):
+    print >> where, '[%s] %s' % (datetime.now().strftime('%H:%M:%S.%f'), message)
+
+def log(mode, message):
+    if mode == 0:
+        _log(message)
+    elif mode == 1:
+        if flag == 1:
+            _log(message)
+    elif mode == 2:
+        _log(message, stderr)
+
 
 class WorkerThread(Thread):
     def __init__(self, n, q):
         self.n = n
         self.q = q
         Thread.__init__(self)
-        print 'Worker #%d init' % n
+        log(DEBUG, 'Worker #%d init' % n)
 
     def run(self):
-        print 'Worker #%d started' % self.n
+        log(DEBUG, 'Worker #%d started' % self.n)
         while True:
             conn, addr = self.q.get(block=True)
-            print 'Worker #%d: Accept new task' % self.n
+            log(DEBUG, 'Worker #%d: Accept new task' % self.n)
             cont = ''
             try:
                 while True:
@@ -78,13 +98,13 @@ class WorkerThread(Thread):
             m = REGEX_PROXY_CONNECTION.search(cont)
             if not m:
                 self.q.task_done()
-                print '!!! Worker #%d: Task reject' % self.n
+                log(DEBUG, '!!! Worker #%d: Task reject' % self.n)
                 return
 
             req = cont.split('\r\n')
             if len(req) < 4:
                 self.q.task_done()
-                print '!!! Worker #%d: Task reject' % self.n
+                log(DEBUG, '!!! Worker #%d: Task reject' % self.n)
                 return
             head = req[0].split(' ')
             phost = False
@@ -104,7 +124,7 @@ class WorkerThread(Thread):
                 phost = '127.0.0.1'
             path = head[1][len(phost)+7:]
 
-            print 'Worker #%d: Process - %s' % (self.n, req[0])
+            log(DEBUG, 'Worker #%d: Process - %s' % (self.n, req[0]))
 
             new_head = ' '.join([head[0], path, head[2]])
 
@@ -145,25 +165,31 @@ class WorkerThread(Thread):
             req_sc.close()
             conn.close()
 
-            print 'Worker #%d: Task done' % self.n
+            log(DEBUG, 'Worker #%d: Task done' % self.n)
             self.q.task_done()
 
 
 class Server(object):
-    def __init__(self, hostname, port):
+    def __init__(self, hostname, port, count):
         self.hostname = hostname
         self.port = port
+        self.count = count
         self.q = Queue()
 
     def start(self):
-        for i in range(1, 65):
-            th = WorkerThread(i, self.q)
+        for i in range(0, self.count):
+            th = WorkerThread(i + 1, self.q)
             th.daemon = True
             th.start()
             sleep(0.01)
         self.sc = socket(AF_INET, SOCK_STREAM)
         self.sc.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self.sc.bind((self.hostname, self.port))
+        try:
+            self.sc.bind((self.hostname, self.port))
+        except error as e:
+            log(ERROR, '!!! Fail to bind server at [%s:%d]: %s' % (self.hostname, self.port, e.args[1]))
+            return
+        log(INFO, 'Server binded at [%s:%d]. Listen.' % (self.hostname, self.port))
         self.sc.listen(10)
 
         while True:
@@ -181,10 +207,19 @@ def main():
                       help='Host to listen [%default]')
     parser.add_option('-p', '--port', type='int', default=8800,
                       help='Port to listen [%default]')
+    parser.add_option('-c', '--count', type='int', default=64,
+                      help='Count of thread to spawn [%default]')
+    parser.add_option('-v', '--verbose', action="store_true",
+                      help='Print verbose')
     options, args = parser.parse_args()
-    if not (0 <= options.port <= 65535):
-        parser.error('port must be 0-65535')
-    server = Server(options.host, options.port)
+    if not (1 <= options.port <= 65535):
+        parser.error('port must be 1-65535')
+    server = Server(options.host, options.port, options.count)
+    global flag
+    if options.verbose:
+        flag = DEBUG
+    else:
+        flag = INFO
     try:
         server.start()
     except KeyboardInterrupt:
